982f1833a962abdb39956f666f66e7fd27572449
Hector Zambrano <elderzambranoperez@gmail.com>
2026-03-11 21:38:31 -0300

    sds

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,14 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        double[] result = new double[length];
 
-        return []; // replace this return statement with your own
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = number * (i + 1);
+        }
+
+        return result;
     }
 
     /// <summary>
@@ -25,9 +27,12 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        for (int i = 0; i < amount; i++)
+        {
+            int last = data[data.Count - 1];
+            data.RemoveAt(data.Count - 1);
+            data.Insert(0, last);
+        }
     }
+
 }
